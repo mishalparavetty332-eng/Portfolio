7aa4c20706a9b0d314b51ed6ba03b23f872c33d7
mishalparavetty332-eng <mishalparavetty332@gmail.com>
2026-05-10 15:59:46 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,22 @@
+// function sendMail() {
+//     let params = {
+//         name: document.getElementById("name").value,
+//         email: document.getElementById("email").value,
+//         message: document.getElementById("message").value,
+//     }
+
+//     const serviceID = "service_cmd7q4b";
+//     const templateID = "template_t5o97nd";
+
+//     emailjs.send(serviceID, templateID, params)
+//         .then(
+//             res => {
+//                 document.getElementById("name").value = "";
+//                 document.getElementById("email").value = "";
+//                 document.getElementById("message").value = "";
+//                 console.log(res);
+//                 alert("Your Message Sent Successfully");
+//             })
+//         .catch((err) => console.log(err));
+// }
+
